f9afa432b12486e99c44058264f37f5eddaf8dab
Malte S. Stretz <stretz@silpion.de>
2015-04-19 00:24:24 +0200

    Add path parameter to BlobResource

--- src/main/java/de/msquadrat/blobwizard/resources/BlobResource.java
+++ src/main/java/de/msquadrat/blobwizard/resources/BlobResource.java
@@ -9,33 +9,37 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.codahale.metrics.annotation.Timed;
 
 import de.msquadrat.blobwizard.BlobStoreManager;
 
-@Path("/blob/{store}")
+@Path("/blob/{store}/{path:.+}")
 @Produces(MediaType.APPLICATION_OCTET_STREAM)
 @Consumes(MediaType.APPLICATION_OCTET_STREAM)
 public class BlobResource {
+    private static final Logger LOGGER = LoggerFactory.getLogger(BlobResource.class);
     
     public BlobResource(BlobStoreManager storeManager) {
     }
     
     @PUT
     @Timed
-    public void putBlob(@PathParam("store") String store) {
+    public void putBlob(@PathParam("store") String store, @PathParam("path") String path) {
         
     }
     
     @GET
     @Timed
-    public Object getBlob(@PathParam("store") String store) {
+    public Object getBlob(@PathParam("store") String store, @PathParam("path") String path) {
         return null;
     }
     
     @DELETE
     @Timed
-    public void deleteBlob(@PathParam("store") String store) {
+    public void deleteBlob(@PathParam("store") String store, @PathParam("path") String path) {
         
     }
 
