dc1cc01ea87e367091caab1322c4af09132c1e06
Malte S. Stretz <stretz@silpion.de>
2015-04-19 00:54:21 +0200

    Implement and delegate to skeleton StoreManager

--- src/main/java/de/msquadrat/blobwizard/BlobStoreManager.java
+++ src/main/java/de/msquadrat/blobwizard/BlobStoreManager.java
@@ -1,21 +1,74 @@
 package de.msquadrat.blobwizard;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang3.NotImplementedException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.dropwizard.lifecycle.Managed;
 
 public class BlobStoreManager implements Managed {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(BlobStoreManager.class);
+    
+    final Map<String, Store> stores = new HashMap<>();
+    
+    
     public BlobStoreManager(ServerConfiguration config) {
-        // TODO
+        for (Map.Entry<String, ServerConfiguration.Store> store : config.getStores().entrySet()) {
+            stores.put(store.getKey(), new Store(store.getValue()));
+        }
     }
     
     @Override
     public void start() throws Exception {
-        
+        for (Store store : stores.values()) {
+            store.start();
+        }
     }
 
     @Override
     public void stop() throws Exception {
-        
+        for (Store store : stores.values()) {
+            store.start();
+        }
+    }
+    
+    public Store get(String name) {
+        return stores.get(name);
     }
 
+    
+    public class Store implements Managed {
+        public Store(ServerConfiguration.Store config) {
+            // TODO
+        }
+        
+        @Override
+        public void start() throws Exception {
+        }
+
+        @Override
+        public void stop() throws Exception {
+        }
+        
+        @Deprecated
+        private Object notImplemented(String method, String path) {
+            LOGGER.info("{} <{}>", method, path);
+            throw new NotImplementedException(method);
+        }
+        
+        public void put(String path, Object data) {
+            notImplemented("PUT", path);
+        }
+        
+        public Object get(String path) {
+            return notImplemented("GET", path);
+        }
+        
+        public void delete(String path) {
+            notImplemented("DELETE", path);
+        }
+    }
 }
--- src/main/java/de/msquadrat/blobwizard/resources/BlobResource.java
+++ src/main/java/de/msquadrat/blobwizard/resources/BlobResource.java
@@ -2,48 +2,50 @@ package de.msquadrat.blobwizard.resources;
 
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
-import org.apache.commons.lang3.NotImplementedException;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.codahale.metrics.annotation.Timed;
 
 import de.msquadrat.blobwizard.BlobStoreManager;
+import de.msquadrat.blobwizard.BlobStoreManager.Store;
 
 @Path("/blob/{store}/{path:.+}")
 public class BlobResource {
-    private static final Logger LOGGER = LoggerFactory.getLogger(BlobResource.class);
+    private final BlobStoreManager stores;
     
     public BlobResource(BlobStoreManager storeManager) {
+        stores = storeManager;
     }
     
-    @Deprecated
-    private Object notImplemented(String method, String store, String path) {
-        LOGGER.info("{} on store <{}> via path <{}>", method, store, path);
-        throw new NotImplementedException(method);
+    
+    private Store getStore(String name) {
+        Store store = stores.get(name);
+        if (store == null) {
+            throw new NotFoundException("Unknown BLOB store " + name);
+        }
+        return store;
     }
+    
     
     @PUT
     @Timed
-    public void putBlob(@PathParam("store") String store, @PathParam("path") String path) {
-        notImplemented("PUT", store, path);
+    public void putBlob(@PathParam("store") String store, @PathParam("path") String path, Object data) {
+        getStore(store).put(path, data);
     }
     
     @GET
     @Timed
     public Object getBlob(@PathParam("store") String store, @PathParam("path") String path) {
-        return notImplemented("GET", store, path);
+        return getStore(store).get(path);
     }
     
     @DELETE
     @Timed
     public void deleteBlob(@PathParam("store") String store, @PathParam("path") String path) {
-        notImplemented("DELETE", store, path);
+        getStore(store).delete(path);
     }
 
 }
