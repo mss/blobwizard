b603ec9b7adeacf8e95373bbdc0aeaeadd87b4f8
Malte S. Stretz <stretz@silpion.de>
2015-04-22 00:37:40 +0200

    Add basic test

---
+++ src/test/groovy/de/msquadrat/blobwizard/BasicStoreSpec.groovy
@@ -0,0 +1,57 @@
+package de.msquadrat.blobwizard
+
+import de.msquadrat.blobwizard.ServerConfiguration.*
+import de.msquadrat.blobwizard.BlobStoreManager.*
+
+import spock.lang.*
+import java.nio.file.*
+
+class BasicStoreSpec extends Specification {
+    String containerName
+    Path container
+    BlobStoreManager.Store store
+    def setup() {
+        Path target = Paths.get(this.class.protectionDomain.codeSource.location.toURI())
+        while (target.toFile().name != "target") {
+            target = target.parent
+        }
+        container = Files.createTempDirectory(target, "test-container-")
+        container.toFile().deleteOnExit()
+        containerName = container.toFile().name
+
+        ServerConfiguration.Store config = Stub()
+        config.getApi() >> "filesystem"
+        config.getOptions() >> ["filesystem.basedir": target.toString()]
+        store = new BlobStoreManager.Store("target", config)
+    }
+
+    def "PUT creates file"() {
+        when:
+        store.put(containerName, "GET", new ByteArrayInputStream([ 0x66, 0x6f, 0x6f ] as byte[]))
+
+        then:
+        Files.exists(container.resolve("GET"))
+    }
+
+    def "GET retrieves file"() {
+        setup:
+        Files.write(Files.createFile(container.resolve("GET")), [ 0x66, 0x6f, 0x6f ] as byte[]);
+
+        when:
+        def res = store.get(containerName, "GET")
+
+        then:
+        res.isPresent()
+    }
+
+    def "DELETE purges file"() {
+        setup:
+        Files.write(Files.createFile(container.resolve("DELETE")), [ 0x66, 0x6f, 0x6f ] as byte[]);
+
+        when:
+        def res = store.delete(containerName, "DELETE")
+
+        then:
+        !Files.exists(container.resolve("DELETE"))
+    }
+}
